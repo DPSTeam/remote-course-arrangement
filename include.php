65036abf2b037a14dc61ee10cd0ad1ffce227845
DGideas <dgideas@outlook.com>
2016-02-09 11:15:49 +0800

    build basic structure

---
+++ include.php
@@ -0,0 +1,6 @@
+<?php
+//Author:@DGideas
+//2015-02-09
+
+require("library/curl.php"); // curl 封装
+?>
---
+++ library/curl.php
@@ -0,0 +1,19 @@
+<?php
+//Author:@DGideas
+//2016-02-09
+
+class Curl
+{
+	protected $conf = array();
+	
+	function __construct($url)
+	{
+		
+	}
+	
+	public function args($args, $argType = "POST")
+	{
+		
+	}
+}
+?>
