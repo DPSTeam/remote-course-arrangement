1bd83751a42dda2d4c533c48dfa8e13124f7b4a1
DGideas <dgideas@outlook.com>
2016-02-11 23:06:53 +0800

    sync website

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var data_storage;
 var username;
+var global_token;
 
-function setCookie(c_name,value,expiredays)
+function setCookie(c_name, value, expiredays)
 {
 	var exdate = new Date();
 	exdate.setDate(exdate.getDate() + expiredays);
@@ -11,11 +12,11 @@ function setCookie(c_name,value,expiredays)
 
 function getCookie(c_name)
 {
-	if (document.cookie.length>0)
+	if (document.cookie.length > 0)
 	{
 		c_start = document.cookie.indexOf(c_name + "=");
 		if (c_start != -1)
-		{ 
+		{
 			c_start = c_start + c_name.length + 1 ;
 			c_end = document.cookie.indexOf(";", c_start);
 			if (c_end == -1)
@@ -23,18 +24,13 @@ function getCookie(c_name)
 				c_end = document.cookie.length;
 			}
 			return unescape(document.cookie.substring(c_start, c_end));
-		} 
+		}
 	}
 	return "";
 }
 
 $(document).ready(function()
 {
-	$.post("//121.42.141.42/dps/banner.html", {},
-		function(data, status){
-			data_storage = data;
-			$("#banner").html(data_storage);
-		});
 	username = getCookie('username');
 	if (username == null || username == "")
 	{
@@ -42,12 +38,14 @@ $(document).ready(function()
 			function(data, status){
 				data_storage = data;
 				$("#main").html(data_storage);
-				$("#status").html("用户登录");
-				$(".banner-detail").toggleClass("hidden");
 			});
 	}
 	else
 	{
 		
 	}
+	
+	$("#system-slogan").click(function(){
+		window.location.reload();
+	});
 });
